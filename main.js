e01091961dac9cc1ab93b044a3d1a9a016d4adc4
Zachary <ZacharyZhang1990@gmail.com>
2018-08-23 09:47:26 +0800

    修改文件

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
-var yyy=document.getElementById('xxx');
+var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
+var lineWidth = 5;
 autoSetCanvasSize(yyy)
-listenToMouse(yyy)
+listenToUser(yyy)
+var eraserEnabled = false
 /*************/
-
-var eraserEnabled = false;
-eraser.onclick = function() {
-  eraserEnabled = true;
-  actions.className = "actions x"
-};
-brush.onclick = function() {
+pen.onclick = function() {
   eraserEnabled = false;
-  actions.className = "actions"
+  pen.classList.add("active")
+  eraser.classList.remove("active")
 };
-
-/**********/
+eraser.onclick = function() {
+   eraserEnabled = true;
+   eraser.classList.add("active")
+   pen.classList.remove("active")
+ };
+ clear.onclick = function() {
+  context.clearRect(0, 0, yyy.width, yyy.height);
+ }
+ download.onclick = function() {
+   var url = yyy.toDataURL("image/png")
+   var a = document.createElement('a')
+   document.body.appendChild(a)
+   a.href = url
+   a.download = 'myPicture'
+   a.target = '_blank'
+   a.click()
+ }
+ red.onclick = function(){
+   context.fillStyle = 'red'
+   context.strokeStyle = 'red'
+   red.classList.add("active")
+   green.classList.remove("active")
+   blue.classList.remove("active")
+ }
+ green.onclick = function(){
+   context.fillStyle = 'green'
+   context.strokeStyle = 'green'
+   red.classList.remove("active")
+   green.classList.add("active")
+   blue.classList.remove("active")
+ }
+ blue.onclick = function(){
+   context.fillStyle = 'blue'
+   context.strokeStyle = 'blue'
+   red.classList.remove("active")
+   green.classList.remove("active")
+   blue.classList.add("active")
+ }
+ thin.onclick = function () {
+   lineWidth = 5
+ }
+ thick.onclick = function () {
+  lineWidth = 10
+}
 function autoSetCanvasSize(canvas){
     setCanvasSize()
   window.onresize = function(){
@@ -28,25 +67,48 @@ function autoSetCanvasSize(canvas){
   }
 }
 /*********/
-  function drawCircle(x,y,radius) {
-    context.beginPath();
-    context.fillStyle = 'black';
-    context.arc(x,y,radius,0,Math.PI * 2);
-    context.fill();
-  }
   function drawLine(x1,y1,x2,y2) {
     context.beginPath();
-    context.strokeStyle = 'black';
     context.moveTo(x1,y1);
-    context.lineWidth = 5;
+    context.lineWidth = lineWidth;
     context.lineTo(x2,y2);
     context.stroke();
     context.closePath();
   }
 /*************/
-function listenToMouse(canvas){
-  var using = false;
-  var lastPoint = {x:undefined, y:undefined };
+function listenToUser(canvas) {
+    var using = false;
+    var lastPoint = {x:undefined, y:undefined };
+    if (document.body.ontouchstart !== undefined){
+    canvas.ontouchstart = function(aaa){
+    var x = aaa.touches[0].clientX;
+    var y = aaa.touches[0].clientY;
+    using = true;
+    if (eraserEnabled) {
+    context.clearRect(x-5,y-5,10,10);
+    }
+    else{
+      lastPoint = {"x":x,"y":y};
+    }
+  };
+  canvas.ontouchmove = function(aaa){
+     var x = aaa.touches[0].clientX;
+     var y = aaa.touches[0].clientY;
+     var newPoint = {"x":x,"y":y};
+     if(!using){
+        return
+     }
+     if(eraserEnabled){
+            context.clearRect(x-5,y-5,10,10);
+     }else{
+ drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
+        lastPoint = newPoint;
+       }
+  };
+  canvas.ontouchend = function(aaa){
+    using = false;
+  };
+}else{
   canvas.onmousedown = function(aaa){
     var x = aaa.clientX;
     var y = aaa.clientY;
@@ -75,4 +137,5 @@ function listenToMouse(canvas){
   canvas.onmouseup = function(aaa){
     using = false;
   };
+  }
 }
